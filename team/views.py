bbc5513c04265a84dbad9e27df5efac65960d166
esadakman <esadd26@gmail.com>
2022-08-27 16:33:47 +0300

    v3

--- team/views.py
+++ team/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from team.models import Student
 from .forms import StudentForm
 from django.contrib import messages
 
 
+def home_page(request):
+    return render(request, 'team/base.html')
+
+
 def student_page(request):
     form = StudentForm()
-    query = request.POST
     print(request.POST)
     if request.method == 'POST':
-        form = StudentForm(request.POST, request.FILES)
+        form = StudentForm(request.POST)
         if form.is_valid:
             form.save()
             messages.success(request, 'Student Added Succesfully')
+            return redirect('')
     context = {
-        'form': form,
-        'query': query
+        'form':form
     }
     return render(request, 'team/student_form.html', context)
-
 
 def student_update(request):
     pass
